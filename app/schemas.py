b3f81a950d863fb1384e6fd980fabf66a1132388
braveseokyung <sk100@korea.ac.kr>
2024-10-03 14:55:17 +0900

    fast api 기본 세팅 완료

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,18 @@
 # database
 from pydantic import BaseModel
+from typing import List
+
+
+class NutrientSchema(BaseModel):
+    id: int
+    NUT_NM: str
+    NUT_FNCLTY: str
+    PRDCT_TITLE: str
+    PRDCT_LINK: str
+    PRDCT_IMG: str
+    PRDCT_PRICE: int
+    MJR_CATEGORY: List[str]  # ARRAY 필드이므로 List로 설정
+
+    class Config:
+        orm_mode = True 
 

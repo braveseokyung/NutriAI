4db24c63855d4ff4a72c9f1f68bc4188bfb4929a
braveseokyung <sk100@korea.ac.kr>
2024-10-03 14:55:17 +0900

    fast api 기본 세팅 완료

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
+from .schemas import NutrientSchema
+from .models import Nutrient
 
 from . import models, schemas
 from app.database import SessionLocal, engine
@@ -8,12 +11,21 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+@app.get("/get_db")
 def get_db():
     db = SessionLocal()
+    
     try:
         yield db
     finally:
         db.close()
+
+@app.get("/data", response_model=List[NutrientSchema])  # Adjust response_model to match your pydantic model
+def get_all_data(db: Session = Depends(get_db)):
+    data = db.query(Nutrient).all()
+    return data
+
 
 @app.get("/")
 def hello():
--- app/models.py
+++ app/models.py
@@ -7,12 +7,13 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 
 class Nutrient(Base):
-    id = Column(Integer, primary_key=True)
-    NUT_NM = Column(TEXT, unique=False, index=True)
+    __tablename__="main_final"
+    id = Column(Integer, primary_key=True, index=True)
+    NUT_NM = Column(TEXT, unique=False, index=False)
     NUT_FNCLTY = Column(TEXT, unique=False, index=False)
     PRDCT_TITLE = Column(VARCHAR, unique=False, index=False)
     PRDCT_LINK = Column(TEXT, unique=False, index=False)
     PRDCT_IMG = Column(TEXT, unique=False, index=False)
     PRDCT_PRICE = Column(TEXT, unique=False, index=False)
-    MJR_CATEGORY = Column(ARRAY, unique=False, index=False)
+    MJR_CATEGORY = Column(ARRAY(String), unique=False, index=False)
 

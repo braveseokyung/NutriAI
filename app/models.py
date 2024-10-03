5fa1bf2c9597242d13035febdf7587e23ac790cf
braveseokyung <sk100@korea.ac.kr>
2024-10-03 13:07:58 +0900

    fastapi 세팅

---
+++ app/models.py
@@ -0,0 +1,18 @@
+# pydantic
+
+from pydantic import BaseModel 
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, VARCHAR, TEXT, ARRAY
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+class Nutrient(Base):
+    id = Column(Integer, primary_key=True)
+    NUT_NM = Column(TEXT, unique=False, index=True)
+    NUT_FNCLTY = Column(TEXT, unique=False, index=False)
+    PRDCT_TITLE = Column(VARCHAR, unique=False, index=False)
+    PRDCT_LINK = Column(TEXT, unique=False, index=False)
+    PRDCT_IMG = Column(TEXT, unique=False, index=False)
+    PRDCT_PRICE = Column(TEXT, unique=False, index=False)
+    MJR_CATEGORY = Column(ARRAY, unique=False, index=False)
+
---
+++ app/schemas.py
@@ -0,0 +1,3 @@
+# database
+from pydantic import BaseModel
+

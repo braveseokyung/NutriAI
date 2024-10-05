596b22b0ffea74d1077a7aef77fb620a2c58632c
braveseokyung <sk100@korea.ac.kr>
2024-10-05 23:45:22 +0900

    [style] info 정보 json으로 항목별 구분,주석, requirements.txt

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ from sqlalchemy.inspection import inspect
 from typing import List, Dict, Optional
 import uuid
 import json
-from .schemas import NutrientSchema, RESPONSE_1, RESPONSE_2,RESPONSE_3, UserInput, ConversationSchema
-from .models import Nutrient, Conversation
+from .schemas import NutrientSchema, RESPONSE_1, RESPONSE_2,RESPONSE_3, UserInput
+from .models import Nutrient
 from openai import OpenAI
 from sqlalchemy import text
 from sqlalchemy import func, any_
-import supabase
 from supabase import Client, create_client
 
 from . import models
@@ -26,8 +25,6 @@ SUP_API_URL=os.getenv('SUP_API_URL')
 SUP_API_KEY=os.getenv('SUP_API_KEY')
 
 CHAT_MODEL="gpt-4o-2024-08-06"
-
-
 
 supabase_client: Client = create_client(SUP_API_URL, SUP_API_KEY)
 
@@ -46,25 +43,13 @@ def get_db():
         db.close()
 
 
+# Nutrient 테이블의 모든 데이터를 반환하는 함수
 @app.get("/data", response_model=List[NutrientSchema])  # Adjust response_model to match your pydantic model
 def get_all_data(db: Session = Depends(get_db)):
     data = db.query(Nutrient).all()
     return data
 
-# @app.get("/user", response_model=List[ConversationSchema])  # Adjust response_model to match your pydantic model
-# def get_user_data(db: Session = Depends(get_db)):
-#     data = db.query(Conversation).all()
-#     return data
-
-# @app.post("/create/conversation")
-# async def create_conversation(user_id : uuid, db: Session = Depends(get_db)):
-#     create_user_model = models.User()
-#     create_user_model.user_id=user_id
-#     create_user_model.conversation=[]
-
-#     db.add(create_user_model)
-#     db.commit()
-
+# nutrient 정보를 dictionary로 반환 (아래 category 함수의 형식 지정에 필요)
 def nutrient_to_dict(nutrient):
     return {
         "NUT_NM": nutrient.NUT_NM,
@@ -76,7 +61,7 @@ def nutrient_to_dict(nutrient):
         "PRDCT_PRICE": nutrient.PRDCT_PRICE
     }
 
-
+# Nutrient 테이블에서 category에 해당하는 데이터를 return
 @app.get("/category/{category}")
 def get_data_by_category(category: str):
     # MJR_CATEGORY 배열에서 category 값이 포함된 행을 필터링
@@ -87,18 +72,16 @@ def get_data_by_category(category: str):
     db.close()
 
     nutrient_list = [nutrient_to_dict(nutrient) for nutrient in results]
-
-    # results_json=json.loads(results)
     
     return str(nutrient_list)
 
-# 대화 상태를 저장하는 딕셔너리 (실제 운영 환경에서는 데이터베이스 사용 권장)
-# conversations: Dict[str, List[Dict[str, str]]] = {}
 
+# conversation : 가장 메인, 여기서 모든 llm 모델을 사용해서 응답을 return
 @app.post("/conversation")
 def conversation(user_prompt: UserInput , conversation_id: Optional[str] = Query(None)):
+    # conversation id를 입력하지 않거나 conversation table에 id가 없으면
     if conversation_id is None or supabase_client.table("conversation").select("*").eq("id", conversation_id) is None:
-        # 새로운 대화 시작
+        # 새로운 대화 시작 (대화 id 생성)
         conversation_id = str(uuid.uuid4())
         conversation = list()
 
@@ -109,26 +92,24 @@ def conversation(user_prompt: UserInput , conversation_id: Optional[str] = Query
         }
 
         conversation.append(conversation_system)
-        # conversation_json = json.dumps(conversation)
-
+       
+        # 새로운 conversation conversation DB에 추가
         supabase_client.table("conversation").insert({
             "id": conversation_id,
             "conversation": conversation
         }).execute()
         
     else:
-        # return {"end"}
-        # 기존 대화 이어나가기
-        # conversation = conversations[conversation_id]
+        
+        # DB에서 기존 conversation conversation id를 가지고 불러오기
         res = supabase_client.table("conversation").select("*").eq("id", conversation_id).execute()
-        # return res.data[0]['conversation']
         conversation = res.data[0]['conversation']
 
     # 사용자 메시지 추가
     conversation.append(dict({"role": "user", "content": user_prompt.message}))
 
     # 대화 처리 및 응답 생성
-    user_input_type, answer = chatbot_ask_symptom(user_prompt.message, conversation)
+    user_input_type, answer = chatbot_ask_symptom(conversation)
 
     # 어시스턴트 응답 추가
     # conversation.append({"role": "assistant", "content": answer})
@@ -138,48 +119,39 @@ def conversation(user_prompt: UserInput , conversation_id: Optional[str] = Query
         "conversation": conversation
     }).eq("id", conversation_id).execute()
 
-    # 대화 상태 저장
-    # conversations[conversation_id] = conversation
-
     # 대화 종료 여부 확인
     if user_input_type == 2:
         symptom, category = summarize_and_categorize(conversation)
+
+        data_category=get_data_by_category(category)
+        symptom,info_nut_nm, info_nut_fnclty,info_prdct_title,info_prdct_link,info_prdct_image,info_prdct_price=compare_and_recommend(symptom,data_category)
         
-        # 대화 종료: 상태에서 제거
-        # del conversations[conversation_id]
+        # 대화 종료: 디비 conversation 테이블에서 제거
         supabase_client.table("conversation").delete().eq("id", conversation_id).execute()
         
-        data_category=get_data_by_category(category)
-        symptom,info=compare_and_recommend(symptom,data_category)
-
         return {
             "symptom": symptom,
-            "info": info
+            "info_nut_nm": info_nut_nm,
+            "info_nut_fnclty":info_nut_fnclty,
+            "info_prdct_title":info_prdct_title,
+            "info_prdct_link":info_prdct_link,
+            "info_prdct_image":info_prdct_image,
+            "info_prdct_price":info_prdct_price
         }
 
-
+    # user_input_type=1(대화를 지속해야 하는 경우)
+    # conversation_id, user_input_type, response를 return
     return {
         "conversation_id": conversation_id,
         "user_input_type":user_input_type,
         "response": answer
     }
 
-
+# LLM 1 : 증상을 물어보는 LLM
 @app.get("/chat")
-def chatbot_ask_symptom(user_prompt, conversation):
-
-    # res = supabase_client.table("conversation").select("*").eq("id", conversation_id).execute()
-    # conversation = res.data[0]['conversation']
+def chatbot_ask_symptom(conversation):
 
     client=OpenAI(api_key=API_KEY)
-    # conversation=conversations[conversation_id]
-    # res = supabase_client.table("conversation").select("*").eq("id", conversation_id).execute()
-    # conversation = list(res.data[0]['conversation'])
-
-    # conversation=list(conversation)
-
-    # conversation.append({"role": "user", "content": user_prompt})
-    # return conversation
 
     response = client.beta.chat.completions.parse(
         model=CHAT_MODEL,
@@ -197,15 +169,16 @@ def chatbot_ask_symptom(user_prompt, conversation):
         user_input_type = 2  # 기본값으로 종료
         answer = response_content
 
+    # 지속여부(1,2)와 응답을 return
     return user_input_type, answer
 
 
+# LLM 2 : 증상을 요약하고 카테고리로 분류하는 함수
 @app.get("/summarize")
 def summarize_and_categorize(conversation):
 
     CATEGORIES=['인지기능/기억력', '긴장', '수면의 질', '피로', '치아', '눈', '피부', '간', '위', '장', '체지방', '칼슘흡수', '혈당', '갱년기 여성', '갱년기 남성', '월경 전 불편한 상태', '혈중 중성지방', '콜레스테롤', '혈압', '혈행', '면역', '항산화', '관절', '뼈', '근력', '운동수행능력', '전립선', '배뇨', '요로']
     llm_2_role={"role": "system", "content":f"Summarize the symptoms in one line. Also categorize the symptom. The category must belong in {', '.join(CATEGORIES)}"}
-    # conversation=conversations[conversation_id]
     
     conversation.append(llm_2_role)
     
@@ -219,15 +192,14 @@ def summarize_and_categorize(conversation):
     )
 
     response=response.choices[0].message.content
-    # print(response)
     json_response=json.loads(response)
     symptom=json_response['SYMPTOM']
     category=json_response['CATEGORY']
 
+    # 최종 증상과 카테고리(대분류) 반환
     return symptom, category   
 
-# category -> db에서 카테고리에 해당하는 로우를 다 가져옴
-
+# LLM 3: 카테고리에 해당하는 모든 데이터의 증상을 최종 증상과 비교해서 가장 적합한 영양제 정보를 가져오는 함수
 @app.get("/recommend")
 def compare_and_recommend(symptom, data_category):
 
@@ -247,10 +219,15 @@ def compare_and_recommend(symptom, data_category):
     response=response.choices[0].message.content
     json_response=json.loads(response)
     symptom_answer=json_response['SYMPTOM']
-    info_answer=json_response['INFO']
+    info_nut_nm=json_response['INFO_NUT_NM']
+    info_nut_fnclty=json_response['INFO_NUT_FNCLTY']
+    info_prdct_title=json_response['INFO_PRDCT_TITLE']
+    info_prdct_link=json_response['INFO_PRDCT_LINK']
+    info_prdct_image=json_response['INFO_PRDCT_IMAGE']
+    info_prdct_price=json_response['INFO_PRDCT_PRICE']
 
-
-    return symptom_answer, info_answer
+    # 최종 증상 요약 한 줄과 영양제 관련 정보를 출력
+    return symptom_answer, info_nut_nm, info_nut_fnclty, info_prdct_title, info_prdct_link, info_prdct_image, info_prdct_price
 
 
 @app.get("/")
